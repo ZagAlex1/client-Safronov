b5e3065df31aa6afe1fd3ddfd8da1218092a38a6
ZagAlex <zagor85@bk.ru>
2022-07-16 19:14:47 +0300

    Server

--- src/main/java/ru/geekbrains/client/models/Network.java
+++ src/main/java/ru/geekbrains/client/models/Network.java
@@ -9,6 +9,15 @@ import java.net.Socket;
 
 public class Network {
 
+    private static final String AUTH_CMD_PREFIX = "/auth"; // + login + password
+    private static final String AUTHOK_CMD_PREFIX = "/authok"; // + username
+    private static final String AUTHERR_CMD_PREFIX = "/autherr"; // + error message
+    private static final String CLIENT_MSG_CMD_PREFIX = "/cMsg"; // + msg
+    private static final String SERVER_MSG_CMD_PREFIX = "/sMsg"; // + smsg
+    private static final String PRIVATE_MSG_CMD_PREFIX = "/pMsg"; // + pmsg
+    private static final String STOP_SERVER_MSG_CMD_PREFIX = "/stop";
+    private static final String END_CLIENT_MSG_CMD_PREFIX = "/end";
+
     public static final String DEFAULT_HOST = "localhost";
     public static final int DEFAULT_PORT = 8186;
     private DataInputStream in;
---
+++ src/main/java/ru/geekbrains/server/ServerApp.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.server;
+
+import java.io.IOException;
+
+public class ServerApp {
+
+    private final static int DEFAULT_PORT = 8186;
+
+    public static void main(String[] args) {
+
+        try {
+            new MyServer(DEFAULT_PORT).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
